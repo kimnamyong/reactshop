6058a2c4d7d9c1410e63c14a6d2846ea87ff753b
DESKTOP-6ELRJLQ\USER <knyatom@daum.net>
2024-02-27 10:39:10 +0900

    111

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,12 +1,40 @@
 import React from "react";
 import NavBar from "./components/NavBar";
+import { Container } from "react-bootstrap";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-     <NavBar />
+      <NavBar />
+
+      <Container className="background">
+        <h1>20% Season Off</h1>
+        <p> welcome to my shopMall !!!</p>
+      </Container>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="shoes1.jpg" width="100%" alt=""/>
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+
+          <div className="col-md-4">
+          <img src="shoes2.jpg" width="100%" alt=""/>
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+
+          <div className="col-md-4">
+          <img src="shoes3.jpg" width="100%" alt=""/>
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
--- shop/src/components/NavBar.js
+++ shop/src/components/NavBar.js
@@ -4,7 +4,7 @@ function NavBar() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">React 쇼핑몰</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
