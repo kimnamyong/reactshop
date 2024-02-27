fb4c83fe0336c14047d1527673942361d2bea3d4
DESKTOP-6ELRJLQ\USER <knyatom@daum.net>
2024-02-27 11:02:20 +0900

    11

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import { Container } from "react-bootstrap";
-import Data from "./components/data"
+import Data from "./components/data";
 import "./App.css";
 
 function App() {
- let [shoes, shoes변경] = useState(Data);
+  let [shoes, shoes변경] = useState(Data);
 
   return (
     <div className="App">
@@ -17,30 +17,25 @@ function App() {
       </Container>
       <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="shoes1.jpg" width="100%" alt=""/>
-            <h4>{shoes[0].title}</h4>
-            <p>{shoes[0].content} &
-            {shoes[0].price}원
-            </p>
-          </div>
-
-          <div className="col-md-4">
-          <img src="shoes2.jpg" width="100%" alt=""/>
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
-
-          <div className="col-md-4">
-          <img src="shoes3.jpg" width="100%" alt=""/>
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </div>
+          {shoes.map((a, i) => {
+            return <Card shoes={shoes[i]} />;
+          })}
         </div>
-
       </div>
     </div>
   );
 }
 
 export default App;
+
+function Card(props) {
+  return (
+    <div className="col-md-4">
+      <img src="shoes1.jpg" width="100%" alt="" />
+      <h4>{props.shoes.title}</h4>
+      <p>
+        {props.shoes.content} &{props.shoes.price}원
+      </p>
+    </div>
+  );
+}
