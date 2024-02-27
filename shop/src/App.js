9233af874de4812b97919c9c6a88e1888cabaa18
DESKTOP-6ELRJLQ\USER <knyatom@daum.net>
2024-02-27 10:50:04 +0900

    1234

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import NavBar from "./components/NavBar";
 import { Container } from "react-bootstrap";
-
+import Data from "./components/data"
 import "./App.css";
 
 function App() {
+ let [shoes, shoes변경] = useState(Data);
+
   return (
     <div className="App">
       <NavBar />
@@ -17,8 +19,10 @@ function App() {
         <div className="row">
           <div className="col-md-4">
             <img src="shoes1.jpg" width="100%" alt=""/>
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].content} &
+            {shoes[0].price}원
+            </p>
           </div>
 
           <div className="col-md-4">
