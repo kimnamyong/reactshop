39f14430d7d1b71a7cfc541947684485f17ad70c
DESKTOP-6ELRJLQ\USER <knyatom@daum.net>
2024-02-28 10:08:57 +0900

    axios

--- shop/src/components/Home.js
+++ shop/src/components/Home.js
@@ -1,9 +1,10 @@
 //import { Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Card from "./Card";
+import axios from 'axios'
 
 function Home(props) {
-  console.log(props);
+ // console.log(props);
   return (
     <>
       <Container className="background mt-4">
@@ -17,6 +18,26 @@ function Home(props) {
             return <Card shoes={props.shoes[i]} i={i} key={i} />;
           })}
         </div>
+         <button className="btn btn-primary" 
+         onClick={()=>{ 
+            axios.get("https://codingapple1.github.io/shop/data2.json")
+            .then((result)=>{ console.log(result.data)})
+            .catch(()=>{ console.log("접속실패....")});
+          }}> 
+         더보기(axios)
+         </button> 
+         <button className="btn btn-danger"
+          onClick={()=>{
+            fetch("https://codingapple1.github.io/shop/data2.json")
+            .then((result)=>{
+              console.log(result);
+              return result.json()
+            }).then((data)=>{
+              console.log(data)
+            })
+          }}
+         >더보기(fetch)</button>
+
       </div>
     </>
   );
